f17c1690f1c11f58c8fedf3d20ec4e8f2ce84ed4
dd8bb <dtolosa.93@gmail.com>
2023-03-06 17:01:41 +0100

    cmake improved & mingw test
    $ Minor changes on cmake for working with mingw

--- argp/config.h
+++ argp/config.h
@@ -22,27 +22,27 @@
 #define HAVE_STRCASE_H 0
 #define HAVE_STRCHRNUL_H 0
 #define HAVE_STRNDUP_H 0
-#define HAVE_SYSEXITS_H 1
+#define HAVE_SYSEXITS_H 0
 #define HAVE_UNISTD_H 1
 
 /* Unlocked variants of stdio functions */
-#define HAVE_DECL_PUTC_UNLOCKED 1
+#define HAVE_DECL_PUTC_UNLOCKED 0
 #define HAVE_DECL_FPUTS_UNLOCKED 0
 #define HAVE_DECL_FWRITE_UNLOCKED 0
 
 /* strerror_r and strerror */
-#define HAVE_STRERROR_R 1
-#define HAVE_DECL_STRERROR_R 1
+#define HAVE_STRERROR_R 0
+#define HAVE_DECL_STRERROR_R 0
 #define HAVE_DECL_STRERROR 1
 
 /* Miscellaneous functions */
 #define HAVE_ASPRINTF 1
-#define HAVE_MEMPCPY 0
-#define HAVE_RANDOM 1
+#define HAVE_MEMPCPY 1
+#define HAVE_RANDOM 0
 #define HAVE_SLEEP 1
 #define HAVE_STRCASECMP 1
 #define HAVE_STRCHRNUL 0
-#define HAVE_STRNDUP 1
+#define HAVE_STRNDUP 0
 
 /* Variables */
 #define HAVE_DECL_PROGRAM_INVOCATION_SHORT_NAME 0
